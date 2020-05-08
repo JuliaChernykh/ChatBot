34ec0a457d390aa2c2c3a354821b994655a55acd
Julia <52479551+JuliaChernykh@users.noreply.github.com>
2020-05-08 19:36:43 +0300

    Step 9
    Исправлены ошибки в работе:
    1) Бот не ломается при получении литерала от пользователя, если ожидает получить число
    2) Налажена одновременная работа с несколькими пользователями

--- Bot/Main1.py
+++ Bot/Main1.py
@@ -3,8 +3,10 @@ from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 import json
 import database
 
-flag = []  # добавляем id пользователей в флаг, чтобы потом проверять их наличие и использовать это как флаг
-flag1 = []
+available_nums = ['1', '2', '5', '6', '9', '11', '13', '16', '19']  # доступные номера заданий
+answer = {} # в словарь записывается пара (пользователь, отвечающий на задачу) - (ответ на задачу)
+flag = []  # флаг на ожидание ответа
+flag1 = []  # флаг на ожидание номера задания
 token = "caa664b4ef024a1ec8c4d9aa7755bccd8fb87a37c71cdf34cd89bf821dac847358c7b2918c9b8b8af14ba"
 vk = vk_api.VkApi(token=token)
 
@@ -19,7 +21,7 @@ keyboard = {
             "action": {
                 "type": "text",
                 "payload": "{\"button\": \"1\"}",
-                "label": "Дай-ка задачку, неудачник"
+                "label": "Получить задачу"
             },
             "color": "primary"
         },
@@ -27,7 +29,7 @@ keyboard = {
                 "action": {
                     "type": "text",
                     "payload": "{\"button\": \"2\"}",
-                    "label": "Скажи ответ, нахал"
+                    "label": "Получить ответ"
                 },
                 "color": "primary"
             # },
@@ -62,29 +64,30 @@ while True:
             if event.object.text.lower() == "привет":
                 vk.method("messages.send", {"peer_id": event.object.peer_id, "message": "Приветики!", "random_id": 0,
                                             "keyboard": keyboard})
-            elif event.object.text.lower() == "дай-ка задачку, неудачник":
+            elif event.object.text.lower() == "получить задачу":
                 vk.method("messages.send", {"peer_id": event.object.peer_id, "message": "Какую?", "random_id": 0,
                                             "keyboard": keyboard})
                 flag1.append(event.object.peer_id)
-            elif event.object.peer_id in flag1:
+            elif event.object.peer_id in flag1 and event.object.text in available_nums:
                 number = int(event.object.text)
-                answer = database.random_exc(number)
-                vk.method("messages.send", {"peer_id": event.object.peer_id, "message": answer[0], "random_id": 0,
+                answer[event.object.peer_id] = database.random_exc(number)
+                vk.method("messages.send", {"peer_id": event.object.peer_id, "message": answer[event.object.peer_id][0], "random_id": 0,
                                             "keyboard": keyboard})
                 del flag1[flag1.index(event.object.peer_id)]
-                flag.append(event.object.peer_id)
-            elif event.object.peer_id in flag and event.object.text.lower() == answer[1].lower():
-                vk.method("messages.send", {"peer_id": event.object.peer_id, "message": "А ты мне нравишься!", "random_id": 0,
+            elif event.object.peer_id in flag1 and event.object.text not in available_nums:
+                vk.method("messages.send", {"peer_id": event.object.peer_id, "message": 'Данного задания нет в базе', "random_id": 0,
                                             "keyboard": keyboard})
-                del flag[flag.index(event.object.peer_id)]
-            elif event.object.peer_id in flag and event.object.text.lower() == 'скажи ответ, нахал':
+            elif event.object.peer_id in answer.keys() and event.object.text.lower() == answer[event.object.peer_id][1].lower():
+                vk.method("messages.send", {"peer_id": event.object.peer_id, "message": "Верно!", "random_id": 0,
+                                            "keyboard": keyboard})
+                answer.pop(event.object.peer_id)
+            elif event.object.peer_id in answer.keys() and event.object.text.lower() == 'получить ответ':
                 vk.method("messages.send",
-                          {"peer_id": event.object.peer_id, "message": answer[1] + ', дурачок', "random_id": 0,
+                          {"peer_id": event.object.peer_id, "message": answer.pop(event.object.peer_id)[1], "random_id": 0,
                            "keyboard": keyboard})
-                del flag[flag.index(event.object.peer_id)]
-            elif event.object.peer_id in flag and event.object.text.lower() != answer[1].lower():
+            elif event.object.peer_id in answer.keys() and event.object.text.lower() != answer[event.object.peer_id][1].lower():
                 vk.method("messages.send",
-                          {"peer_id": event.object.peer_id, "message": 'Ха, лох',
+                          {"peer_id": event.object.peer_id, "message": 'Неверный ответ',
                            "random_id": 0,
                            "keyboard": keyboard})
             # elif "Negative" in event.object.text:
